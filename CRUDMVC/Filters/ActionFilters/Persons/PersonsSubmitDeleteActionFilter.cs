a965f52ad3ab266e37e5d9ba0bb470a8650d8e7d
Matin <matinnjt2000@gmail.com>
2025-05-10 18:08:13 +0330

    feat(action-filter): add PersonsSubmitDeleteActionFilter for validation in Delete action

---
+++ CRUDMVC/Filters/ActionFilters/Persons/PersonsSubmitDeleteActionFilter.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using ServiceContracts;
+
+namespace CRUDMVC.Filters.ActionFilters;
+
+public class PersonsSubmitDeleteActionFilter : ActionFilterAttribute
+{
+    private readonly ILogger<PersonsSubmitDeleteActionFilter> _logger;
+    private readonly IPersonsService _personsService;
+
+    public PersonsSubmitDeleteActionFilter(ILogger<PersonsSubmitDeleteActionFilter> logger,
+        IPersonsService personsService)
+    {
+        _logger = logger;
+        _personsService = personsService;
+    }
+
+    public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        if (!context.ActionArguments.ContainsKey("personID") ||
+            context.ActionArguments["personID"] is not Guid personID ||
+            personID == Guid.Empty)
+        {
+            _logger.LogError("Invalid or missing personID.");
+            context.Result = new BadRequestObjectResult("Invalid or missing personID.");
+            return;
+        }
+
+        var personResponse = await _personsService.GetPersonByPersonID(personID);
+        if (personResponse == null)
+        {
+            _logger.LogWarning("Person with personID: {personID} not found.", personID);
+            context.Result = new NotFoundObjectResult("Person not found.");
+            return;
+        }
+
+        await next();
+    }
+}
